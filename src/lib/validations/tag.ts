7501a42a43e1c34a734a0236d36b0ae7501ea272
JHSeo <qnfqnfqnf@gmail.com>
2022-12-04 22:12:15 +0900

    feat: add tag schema with error message

--- src/lib/validations/tag.ts
+++ src/lib/validations/tag.ts
@@ -1,6 +1,8 @@
 import { z } from 'zod';
 
 export const tag = z.object({
-  name: z.string(),
-  email: z.string().email(),
+  name: z
+    .string({ required_error: 'Name is required.' })
+    .min(2, 'Name must be at least 2 characters.'),
+  email: z.string({ required_error: 'Email is required.' }).email('Email is invalid.'),
 });
